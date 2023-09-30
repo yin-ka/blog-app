c5190227587d914bfb1efa1332b50fb84a50caf7
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-30 22:28:20 +0100

    Make review changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,4 @@
+# app/models/like.rb
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
@@ -6,6 +7,8 @@ class Like < ApplicationRecord
   private
 
   def increment_likes_count
+    return unless new_record? || saved_change_to_attribute?(:post_id)
+
     post.increment!(:likes_counter)
   end
 end
--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:user) { create(:user) }
-  let(:post) { create(:post, author: user) }
+  let(:user) { User.create(name: 'example_user') }
+  let(:post) { Post.create(title: 'Example Post', author: user, likes_counter: 0) }
+  let(:like) { Like.create(author: user, post:) }
 
   describe 'associations' do
     it { should belong_to(:author).class_name('User') }
     it { should belong_to(:post).class_name('Post') }
+  end
+
+  describe 'after_save callback' do
+    it 'should increment the likes_counter on the associated post' do
+      expect do
+        like.save
+        post.reload
+      end.to change { post.likes_counter }.by(1)
+    end
+
+    it 'should not increment the likes_counter if the like is already persisted' do
+      like.save
+      expect do
+        like.save
+        post.reload
+      end.not_to(change { post.likes_counter })
+    end
   end
 end
